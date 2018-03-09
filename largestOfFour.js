1e29cbcf53acbbc875a97d8219b6ad3c82df6273
German Gamboa <germangamboa95@gmail.com>
2018-03-09 00:40:04 -0500

    added reduce solution

--- largestOfFour.js
+++ largestOfFour.js
@@ -1,4 +1,4 @@
-//This is my solution. I guess a better method would be to use reduce. 
+//This is my solution. I guess a better method would be to use reduce.
 function largestOfFour(arr) {
   let four = [];
   arr.forEach(item => {
@@ -6,6 +6,14 @@ function largestOfFour(arr) {
     four.push(item[0]);
   });
   return four;
+}
+
+function largestOfFourV2(arr) {
+  return arr.map(function(group){
+    return group.reduce(function(prev, current) {
+      return (current > prev) ? current : prev;
+    });
+  });
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
