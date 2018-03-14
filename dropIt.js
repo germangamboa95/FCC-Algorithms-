cd1ebcbe053e2c482c02b3fd134625a4aa8d81fb
German Gamboa <germangamboa95@gmail.com>
2018-03-13 21:42:56 -0400

    added second solution

--- dropIt.js
+++ dropIt.js
@@ -7,9 +7,18 @@ function dropElements(arr, func) {
     }
     flag = func(item);
     return flag;
-  
+
   });
   return arr;
 }
+/** this is my second solution just using a while loop. arr.length is important to avoid an endless loop if the array passed
+happens to be empty. I guess this one is not a fully pure function since it alters the original array. **/
+function dropElements(arr, func) {
+  while(arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+  }
+  return arr;
+}
+
 
 dropElements([1, 2, 3], function(n) {return n < 3; });
